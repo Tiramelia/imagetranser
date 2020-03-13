552ff5b268a90cb653917cca75e07755c7003f8c
Tiramelia <tiranmelia@gmail.com>
2020-03-13 14:00:03 +0100

    pickCommand now uses callbacks

--- imagetranser.py
+++ imagetranser.py
@@ -4,12 +4,13 @@ import sys
 import argparse
 import re
 
-def imgmerge(img):
+def imgmerge(img, args):
     trans = createTrans(img.size, 'RGBA')
     transed = Image.blend(img, trans, 0.5)
     return transed
 
-def imgframe(img, frameWidth):
+def imgframe(img, args):
+    frameWidth = args.width
     transed = createTrans((img.size[0] + 2 * frameWidth, img.size[1] + 2 * frameWidth))
     transed.paste(img, (frameWidth, frameWidth))
     return transed
@@ -23,7 +24,10 @@ def createTrans(size, mode='RGB'):
     drawer.rectangle([(0, 2 * BAR_SIZE), (size[0], size[1] - 2 * BAR_SIZE)], "#ffffff")
     return transflag
 
-def colorToTrans(img, colorRange = 0, color = (255,255,255)):
+def colorToTrans(img, args):
+    colorRange = args.threshold
+    color = hexToRGB(args.colorhex)
+
     transed = createTrans(img.size)
 
     pixdata = img.load()
@@ -43,19 +47,28 @@ def colorToTrans(img, colorRange = 0, color = (255,255,255)):
 def hexToRGB(hex):
     return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
 
-def pickCommand(img):
-    if args.command == 'merge':
-        transed = imgmerge(img)
-        transed.save('transmerged.png')
-    elif args.command == 'frame':
-        transed = imgframe(img, args.width)
-        transed.save('transframed.png')
-    elif args.command == 'color':
-        if not re.match('[\da-fA-F]{6}', args.colorhex):
-            print("Invalid color")
-            return None
-        transed = colorToTrans(img, args.threshold, hexToRGB(args.colorhex))
-        transed.save('transedcolor.png')
+def pickCommand(img, args):
+    switch = {
+        'merge' : imgmerge,
+        'frame' : imgframe,
+        'color' : colorToTrans
+    }
+    transformFunction = switch.get(args.command)
+    result = transformFunction(img, args)
+    result.save('transed.png')
+    print('Saved as transed.png')
+
+def colorhex(value):
+    if not re.match('[\da-fA-F]{6}', value):
+        raise argparse.ArgumentTypeError("{0} is  an invalid color hex.".format(value))
+        return None
+    hexToRGB(value)
+    return value
+
+def main():
+    args = parser.parse_args()
+    img = Image.open(args.filepath)
+    pickCommand(img, args)
 
 parser = argparse.ArgumentParser(description="Adding trans flag to images in various ways.", prog="imagetranser")
 subparser = parser.add_subparsers(dest='command', help='For more help type  imagetranser.py command -h')
@@ -69,8 +82,6 @@ merge.add_argument("filepath", type=str, help="Path to the image file.", metavar
 
 frame.add_argument('-w', "--width", metavar="width", nargs='?', type=int, default=10, help="Specifies width of the frame.")
 color.add_argument('-t', '--threshold', type=int, nargs='?', metavar="threshold", default=0, help="Specifies a color threshold.")
-color.add_argument('-c', '--colorhex', metavar="colorhex", nargs='?', type=str, default="ffffff", help="Specifies color e.g ff00ff (without #)")
+color.add_argument('-c', '--colorhex', metavar="colorhex", nargs='?', type=colorhex, default="ffffff", help="Specifies color e.g ff00ff (without #)")
 
-args = parser.parse_args()
-img = Image.open(args.filepath)
-pickCommand(img)
+main()
