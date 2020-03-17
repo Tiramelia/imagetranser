1586624b73f2764a6c7b39261752ae2ce68bfabd
Tiramelia <tiranmelia@gmail.com>
2020-03-17 08:34:00 +0100

    Added description to functions

--- imagetranser.py
+++ imagetranser.py
@@ -4,18 +4,48 @@ import sys
 import argparse
 import re
 
+
 def imgmerge(img, args):
+    """Merges an image with a trans flag.
+    
+    Arguments:
+        img {PIL.Image.Image} -- PIL image object
+        args {whatever parse_args() returns} -- passed arguments
+    
+    Returns:
+        PIL.Image.Image -- PIL image object
+    """
     trans = createTrans(img.size, 'RGBA')
     transed = Image.blend(img, trans, 0.5)
     return transed
 
 def imgframe(img, args):
+    """Adds a trans frame to an image.
+    
+    Arguments:
+        img {PIL.Image.Image} -- PIL image object
+        args {whatever parse_args() returns} -- passed arguments
+    
+    Returns:
+        PIL.Image.Image -- PIL image object
+    """
     frameWidth = args.width
     transed = createTrans((img.size[0] + 2 * frameWidth, img.size[1] + 2 * frameWidth))
     transed.paste(img, (frameWidth, frameWidth))
     return transed
 
 def createTrans(size, mode='RGB'):
+    """Creates image object of a trans flag.
+    
+    Arguments:
+        size {tuple} -- A 2-tuple containing width and height.
+    
+    Keyword Arguments:
+        mode {str} -- PIL image object opening mode (default: {'RGB'})
+    
+    Returns:
+        PIL.Image.Image -- PIL image object 
+    """
     BAR_SIZE = size[1]/5
     transflag = Image.new(mode, size)
     drawer = ImageDraw.Draw(transflag)
@@ -25,6 +55,15 @@ def createTrans(size, mode='RGB'):
     return transflag
 
 def colorToTrans(img, args):
+    """Changes specified colors of an image to trans flag.
+    
+    Arguments:
+        img {PIL.Image.Image} -- PIL image object
+        args {whatever parse_args() returns} -- passed arguments
+    
+    Returns:
+        PIL.Image.Image -- PIL image object
+    """
     colorRange = args.threshold
     color = hexToRGB(args.colorhex)
 
@@ -45,9 +84,21 @@ def colorToTrans(img, args):
     return img
 
 def hexToRGB(hex):
+    """Changes color hex to RGB value.
+    
+    Arguments:
+        hex {str} -- String containing hex number of a color (without #)
+    
+    Returns:
+        tuple -- 3-tuple containing R, G and B values.
+    """
     return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
 
-def pickCommand(img, args):
+def pickCommand():
+    """Takes the passed arguments and choses a function to call.
+    """
+    args = parser.parse_args()
+    img = Image.open(args.filepath)
     switch = {
         'merge' : imgmerge,
         'frame' : imgframe,
@@ -59,16 +110,13 @@ def pickCommand(img, args):
     print('Saved as transed.png') 
 
 def colorhex(value):
+    """Function for checking if passed argument is in correct format.
+    """
     if not re.match('[\da-fA-F]{6}', value):
         raise argparse.ArgumentTypeError("{0} is  an invalid color hex.".format(value))
         return None
     hexToRGB(value)
     return value
-
-def main():
-    args = parser.parse_args()
-    img = Image.open(args.filepath)
-    pickCommand(img, args)
 
 parser = argparse.ArgumentParser(description="Adding trans flag to images in various ways.", prog="imagetranser")
 subparser = parser.add_subparsers(dest='command', help='For more help type  imagetranser.py command -h', required=True)
@@ -84,4 +132,4 @@ frame.add_argument('-w', "--width", metavar="width", nargs='?', type=int, defaul
 color.add_argument('-t', '--threshold', type=int, nargs='?', metavar="threshold", default=0, help="Color threshold.")
 color.add_argument('-c', '--colorhex', metavar="colorhex", nargs='?', type=colorhex, default="ffffff", help="Color hex e.g ff00ff (without #)")
 
-main()
+pickCommand()
